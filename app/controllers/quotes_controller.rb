08326e7aa39ff2079cd8336e9cef155d12a14c14
Rachael Salter <rasalter@gmail.com>
2015-07-10 18:22:42 +0000

    edited quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,20 +1,22 @@
 class QuotesController < ApplicationController
-	def index
-		@quote = Quote.order("RANDOM()").first
-	end
-
-    def quote_params
-    	params.require(:quote).permit(:saying, :author)
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
+  
+  def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
     end
+    redirect_to root_path
+  end
 
-    def create
-    	@quote = Quote.create(quote_params)
-    	if @quote.invalid?
-    		flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-    	end 
-    	redirect_to root_path
-      end 
+  def about
+  end
 
-    def about
-  end 
-end  
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+end
