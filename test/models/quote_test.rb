ffc70b9fa483325bc8290a8528a907bd6cc869e5
Rachael Salter <rasalter@gmail.com>
2015-08-14 18:44:22 +0000

    add initials and id to quotes

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-  	assert true
-  end
+  test "unique_tag" do 
+  	quote = Quote.create(:author => 'Rachael Salter', :saying => 'Til death do us part.')
+  	expected = 'RS#' + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
+  end 
 end
